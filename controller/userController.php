39bb46d4fe49ee48b858a5499ae19e3861dee322
allen19-77 <allen.19@list.ru>
2022-08-11 21:29:52 +0500

    фукции

--- controller/userController.php
+++ controller/userController.php
@@ -1,4 +1,13 @@
 <?php
+function isAgeValid($age)
+{
+    if ($age < 18 ) {
+        alert('Сюда нельзя!');
+        return false;
+    } else {
+        return true;
+    }
+}
 require __DIR__ . '/../model/Database.php';
 function alert($msg) {
     echo "<script type='text/javascript'>alert('$msg');</script>";
@@ -24,48 +33,54 @@ else {
     $age2 = $_POST['age2'];
     $gender2 = $_POST['gender2'];
 
-    if(strlen($password) < 6 && strlen($password12) < 6) {
-        alert('Пароль должен быть минимум 6 символов');
+    //todo: Ахтунг, дублирует форму
+    if (strlen($password) < 6) {
+        alert('Пароль 1 должен быть минимум 6 символов');
         //echo 'Привет 6 символов';
         include_once __DIR__ . '/../view/registation.php';
     }
-    else {
-        if($password !== $password2 && $password12 !== $password22) {
-            alert('Пароли не совпадают, пожалуйста заполните форму еще раз');
-           // echo 'Пароли не совпали';
-            include_once __DIR__ . '/../view/registation.php';
-        }
-        else {
-            if ($age < 18 && $age2 < 18 ) {
-               alert('Сюда нельзя!');
-            }
-            else {
-                $sql = 'INSERT INTO users (id, login, password, name, age, gender) 
-                        VALUES (NULL, :login, :password, :name, :age, :gender)';
-                $stmt = $db->dbh->prepare($sql);
-                $result = $stmt->execute([
-                    'login' => $login,
-                    'name' => $name,
-                    'password' => $password,
-                    'age' => $age,
-                    'gender' => $gender,
-                ]);
-
-                $sql = 'INSERT INTO users (id, login, password, name, age, gender) 
-                        VALUES (NULL, :login, :password, :name, :age, :gender)';
-                $stmt = $db->dbh->prepare($sql);
-                $result = $stmt->execute([
-                    'login' => $login2,
-                    'name' => $name2,
-                    'password' => $password12,
-                    'age' => $age2,
-                    'gender' => $gender2,
-                ]);
-                alert('Вы успешно зарегились');
-                include_once __DIR__ . '/../view/note.php';
-            }
-        }
+    if (strlen($password12) < 6) {
+        alert('Пароль 2 должен быть минимум 6 символов');
+        //echo 'Привет 6 символов';
+        include_once __DIR__ . '/../view/registation.php';
     }
+    if ($password !== $password2) {
+        alert('Пароли 1 не совпадают, пожалуйста заполните форму еще раз');
+        // echo 'Пароли не совпали';
+        include_once __DIR__ . '/../view/registation.php';
+    }
+    if ($password12 !== $password22) {
+        alert('Пароли 2 не совпадают, пожалуйста заполните форму еще раз');
+        // echo 'Пароли не совпали';
+        include_once __DIR__ . '/../view/registation.php';
+    }
+    isAgeValid($age);
+    isAgeValid($age2);
+
+    //todo: Ахтунг, сохранять пытается в любом случае
+    $sql = 'INSERT INTO users (id, login, password, name, age, gender) 
+            VALUES (NULL, :login, :password, :name, :age, :gender)';
+    $stmt = $db->dbh->prepare($sql);
+    $result = $stmt->execute([
+        'login' => $login,
+        'name' => $name,
+        'password' => $password,
+        'age' => $age,
+        'gender' => $gender,
+    ]);
+
+    $sql = 'INSERT INTO users (id, login, password, name, age, gender) 
+            VALUES (NULL, :login, :password, :name, :age, :gender)';
+    $stmt = $db->dbh->prepare($sql);
+    $result = $stmt->execute([
+        'login' => $login2,
+        'name' => $name2,
+        'password' => $password12,
+        'age' => $age2,
+        'gender' => $gender2,
+    ]);
+    alert('Вы успешно зарегились');
+    include_once __DIR__ . '/../view/note.php';
 }
 
 
