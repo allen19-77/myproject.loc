79b80f42affd170924893cdd4d13a113995c8318
allen19-77 <allen.19@list.ru>
2022-08-18 21:51:11 +0500

    успешный успех

--- controller/userController.php
+++ controller/userController.php
@@ -12,21 +12,21 @@ if (!$db->dbh) {
 }
 else {
     $userOne = new User( $_POST['login'], $_POST['password'], $_POST['password2'], $_POST['name'], $_POST['age'], $_POST['gender']);
-    //$userTwo = new User($_POST['login2'], $_POST['password12'], $_POST['password22'], $_POST['name2'], $_POST['age2'], $_POST['gender2']);
+    $userTwo = new User($_POST['login2'], $_POST['password12'], $_POST['password22'], $_POST['name2'], $_POST['age2'], $_POST['gender2']);
 
 
     //todo: Ахтунг, дублирует форму
 
     $userOne->isPasswordValid();
 
-    //$userTwo->isPasswordValid();
+    $userTwo->isPasswordValid();
 
     $userOne -> isConfirmPasswordValid();
 
-    //$userTwo -> isConfirmPasswordValid();
+    $userTwo -> isConfirmPasswordValid();
 
     $userOne->isAgeValid();
-    //$userTwo->isAgeValid();
+    $userTwo->isAgeValid();
 
     if (!empty($userOne->errors)) {
         foreach ($userOne->errors as $error) {
@@ -49,19 +49,26 @@ else {
     }
 
 //todo: Ахтунг, сохранять пытается в любом случае
-    /*
 
-    $sql = 'INSERT INTO users (id, login, password, name, age, gender)
+    if (!empty($userTwo->errors)) {
+        foreach ($userTwo->errors as $error) {
+            echo $error . "<br>\r\n";
+        }
+        include_once __DIR__ . '/../view/registation.php';
+    } else {
+        $sql = 'INSERT INTO users (id, login, password, name, age, gender)
             VALUES (NULL, :login, :password, :name, :age, :gender)';
-    $stmt = $db->dbh->prepare($sql);
-    $result = $stmt->execute([
-        'login' =>$userTwo->login,
-        'name' =>$userTwo->name,
-        'password' =>$userTwo->password,
-        'age' =>$userTwo->age,
-        'gender' =>$userTwo->gender,
-    ]);*/
-
+        $stmt = $db->dbh->prepare($sql);
+        $result = $stmt->execute([
+            'login' => $userTwo->login,
+            'name' => $userTwo->name,
+            'password' => $userTwo->password,
+            'age' => $userTwo->age,
+            'gender' => $userTwo->gender,
+        ]);
+        echo 'Вы успешно зарегились';
+        include_once __DIR__ . '/../view/note.php';
+    }
 }
 
 
