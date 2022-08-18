573ab5bb77b6af976439761ce81ded48f49f2e25
allen19-77 <allen.19@list.ru>
2022-08-18 21:35:55 +0500

    форму вывели

--- controller/userController.php
+++ controller/userController.php
@@ -12,26 +12,28 @@ if (!$db->dbh) {
 }
 else {
     $userOne = new User( $_POST['login'], $_POST['password'], $_POST['password2'], $_POST['name'], $_POST['age'], $_POST['gender']);
-    $userTwo = new User($_POST['login2'], $_POST['password12'], $_POST['password22'], $_POST['name2'], $_POST['age2'], $_POST['gender2']);
+    //$userTwo = new User($_POST['login2'], $_POST['password12'], $_POST['password22'], $_POST['name2'], $_POST['age2'], $_POST['gender2']);
 
 
     //todo: Ахтунг, дублирует форму
 
     $userOne->isPasswordValid();
 
-    $userTwo->isPasswordValid();
+    //$userTwo->isPasswordValid();
 
     $userOne -> isConfirmPasswordValid();
 
-    $userTwo -> isConfirmPasswordValid();
+    //$userTwo -> isConfirmPasswordValid();
 
     $userOne->isAgeValid();
-    $userTwo->isAgeValid();
+    //$userTwo->isAgeValid();
 
     if (!empty($userOne->errors)) {
         foreach ($userOne->errors as $error) {
             echo $error . "<br>\r\n";
         }
+        include_once __DIR__ . '/../view/registation.php';
+    } else {
 
     }
 
@@ -47,7 +49,7 @@ else {
         'gender' =>$userOne->gender,
     ]);
 
-    $sql = 'INSERT INTO users (id, login, password, name, age, gender) 
+    /*$sql = 'INSERT INTO users (id, login, password, name, age, gender)
             VALUES (NULL, :login, :password, :name, :age, :gender)';
     $stmt = $db->dbh->prepare($sql);
     $result = $stmt->execute([
@@ -56,7 +58,7 @@ else {
         'password' =>$userTwo->password,
         'age' =>$userTwo->age,
         'gender' =>$userTwo->gender,
-    ]);
+    ]);*/
     alert('Вы успешно зарегились');
     include_once __DIR__ . '/../view/note.php';
 }
